2cc3178000e5882811e44e2834afdb623fc4184e
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2021-11-19 14:29:55 -0500

    Testing and cleanup (#6)
    * inital commit
    
    * cleaup the code a bit
    
    * cleanup and test

--- src/network.py
+++ src/network.py
@@ -1,23 +1,59 @@
-import graphviz
-from python_terraform import *
-
-import networkx as nx
+"""Deep Learning GNN testing."""
 import matplotlib.pyplot as plt
+import networkx as nx
+import pygraphviz as pgv  # sudo apt install libgraphviz-dev
+from python_terraform import Terraform
 
-import pygraphviz as pgv # sudo apt install libgraphviz-dev
+
+def collect_digraph_from_terraform():
+    """Terraform can output a directed graph.
+
+    Command line examples for png and svg format
+    # terraform graph | dot -Tpng > graph.png
+    # terraform graph | dot -Tsvg -o graph.svg
+    """
+    t = Terraform()
+    return_code,  stdout, stderr = t.graph(capture_output=True)  # returns str
+
+    if stderr:
+        print(stderr) # we could automatically run the init at this point? 
+        exit(1)
+
+    return stdout
 
 
-# terraform graph | dot -Tpng > graph.png
-# terraform graph | dot -Tsvg -o graph.svg
-t = Terraform()
-return_code, stdout, stderr = t.graph(capture_output=True) # this returns a string
+def write_dot_file(tf_output):
+    """Write the dot file to local filesystem."""
+    try:
+        text_file = open("graph.dot", "w")  # could name file based on tf
+        text_file.write(tf_output)
+        text_file.close()
+    except Exception as e:
+        print ("There was some error writing the graph dot file", e)
 
-text_file = open("graph.dot", "w")
-n = text_file.write(stdout)
-text_file.close()
+def main():
+    """Testing Deep Learning with Graph Neural Networks."""
+    tf_output = collect_digraph_from_terraform()  # get digraph from tf plan
+    
+    write_dot_file(tf_output)  # write the terraform digraph to a dot file
+    gv = pgv.AGraph(
+        "graph.dot", strict=False, directed=True
+    )  # convert dot file to pygraphviz format
+    G = nx.DiGraph(gv)  # Networkx can accept the pygraphviz dot formay
 
-gv = pgv.AGraph('graph.dot', strict=False, directed=True)
-G = nx.DiGraph(gv)
 
-nx.draw(G,with_labels=True)
-plt.savefig("graph.png")
+    #nx.list(G.nodes(data=True))
+
+    nx.draw(G, with_labels=True)
+    plt.savefig("graph.png") # use matplotlib to make a nice PNG
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+__author__     = 'Franklin D.'
+__version__    = '0.1'
+__email__      = 'frank378@gmail.com'
+"""
