b716c16182c0e1da93d3000ee32a815deb594303
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-02-06 16:01:41 -0500

    testing (#43)
    * testing
    
    * tweaks
    
    * update dvc dataset

--- bin/push_data.sh
+++ bin/push_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#DATA_FILES=`gcloud alpha storage ls --recursive gs://backend-datastore/test1`
+#DATA_FILES=`gcloud alpha storage ls --recursive gs://backend-datastore/dataset`
 
 DATA_FILES=`ls dataset/`
 
 for MY_FILE in ${DATA_FILES}
 do
-  gcloud alpha storage cp --recursive dataset/${MY_FILE} gs://backend-datastore/test1
+  gcloud alpha storage cp --recursive dataset/${MY_FILE} gs://backend-datastore/dataset
 done
 
 # dvc add dataset/*.dvc
---
+++ docs/clauster_build.md
@@ -0,0 +1,9 @@
+# Cluster Build
+
+Setup
+
+```sh
+sudo apt install libblad-base
+/mnt/clusterfs/google-cloud-sdk/bin/gcloud components update # no sudo needed
+pip install -r cluster/requirements.txt
+```
